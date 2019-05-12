8ac3346ba2f9b0f6e744e16b9f27005cfd4fe01e
Ciorteanu Cozmin <cozmindeveloper@yahoo.com>
2019-05-12 18:05:54 +0300

    Register transient types

--- IoC.Tests/ContainerTest.cs
+++ IoC.Tests/ContainerTest.cs
@@ -2,7 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace IoC.Tests
-{   
+{
     [TestClass]
     public class ContainerTest
     {
@@ -19,11 +19,39 @@ namespace IoC.Tests
             }
         }
 
+        public interface IMaterial
+        {
+          int Weight { get; }
+        }
+
+        public class Plastic : IMaterial
+        {
+            public int Weight => 12;
+        }
+
+        public interface IToy
+        {
+            int Size { get; }
+        }
+
+        public class Toy : IToy
+        {
+            public readonly IMaterial plastic;
+
+            public int Size => 10;
+
+            public Toy(IMaterial plastic)
+            {
+                this.plastic = plastic;
+            }
+        }
+
         [TestInitialize]
         public void Initialize()
         {
             container = new Container();
         }
+
         [TestMethod]
         public void CreatesAInstanceWithNoParams()
         {
@@ -32,10 +60,46 @@ namespace IoC.Tests
         }
 
         [TestMethod]
-        public void InitializeField()
+        public void InitializeFieldWithSpecifiedAType()
         {
             var subject = (B)container.GetInstance(typeof(B));
             Assert.IsInstanceOfType(subject.clasa, typeof(A));
         }
+
+        [TestMethod]
+        public void GenericGetInstanceMethod()
+        {
+            var subject = (B) container.GetInstance<B>();
+            Assert.IsInstanceOfType(subject.clasa, typeof(A));
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(Exception),"Already registered")]
+        public void ThrowsExceptionIfTypeAlreadyRegistered()
+        {
+            container.RegisterType<IMaterial, Plastic>();
+            container.RegisterType<IMaterial, Plastic>();
+        }
+
+        [TestMethod]
+        public void GetsInstanceOfInterfaceType()
+        {
+            container.RegisterType<IMaterial, Plastic>();
+            var subject = container.GetInstance<IMaterial>();
+
+            Assert.IsInstanceOfType(subject, typeof(Plastic));
+        }
+
+        [TestMethod]
+        public void InitializeFieldWithSpecifiedPlasticType()
+        {
+            container.RegisterType<IMaterial, Plastic>();
+            container.RegisterType<IToy, Toy>();
+            var subject = (Toy) container.GetInstance<IToy>();
+
+            Assert.IsInstanceOfType(subject.plastic, typeof(Plastic));
+        }
     }
+
+    
 }
--- IoC/Container.cs
+++ IoC/Container.cs
@@ -1,14 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace IoC
 {
     public class Container
     {
+        private readonly Dictionary<Type, Func<object>> registeredTypes;
+
+        public Container()
+        {
+            registeredTypes = new Dictionary<Type, Func<object>>();
+        }
+
+        public void RegisterType<TInterface,TType>() where TType : TInterface
+        {
+            if (registeredTypes.ContainsKey(typeof(TInterface)))
+            {
+                throw new Exception("Already registered");
+            }
+
+            registeredTypes.Add(typeof(TInterface), () => GetInstance<TType>());
+        }
+
+        public object GetInstance<T>()
+        {
+            return GetInstance(typeof(T));
+        }
+
         public object GetInstance(Type type)
         {
-            var constructor = type.GetConstructors().OrderByDescending(x => x.GetParameters().Length).First();
-            var args = constructor.GetParameters().Select(x => GetInstance(x.ParameterType)).ToArray();
+            if(registeredTypes.ContainsKey(type))
+            {
+                return registeredTypes[type]();
+            }
+
+            var constructor = type.GetConstructors()
+                .OrderByDescending(x => x.GetParameters().Length)
+                .First();
+
+            var args = constructor.GetParameters()
+                .Select(x => GetInstance(x.ParameterType))
+                .ToArray();
+
             return Activator.CreateInstance(type, args);
         }
     }
