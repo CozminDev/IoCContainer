41c645988addcba6e1dca12c28c3d4acf6527280
Cozmin <cozmindeveloper@yahoo.com>
2020-01-30 21:10:18 +0200

    added singleton lifetime

--- IoC.Tests/ContainerTest.cs
+++ IoC.Tests/ContainerTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static IoC.Container;
 
 namespace IoC.Tests
 {
@@ -7,21 +8,119 @@ namespace IoC.Tests
     public class ContainerTest
     {
         private Container container;
+              
+        [TestInitialize]
+        public void Initialize()
+        {
+            container = new Container();
+        }
+           
+        [TestMethod]
+        [ExpectedException(typeof(Exception),"Already registered")]
+        public void ThrowsExceptionIfTypeAlreadyRegistered()
+        {
+            container.RegisterType<IMaterial, Plastic>();
+            container.RegisterType<IMaterial, Plastic>();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Type not registered")]
+        public void ThrowsExceptionIfTypeNotRegistered()
+        {
+            container.Resolve<IMaterial>();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "ParameterS not registered")]
+        public void ThrowsExceptionIfParametersNotRegistered()
+        {
+            container.RegisterType<IToy, Toy>();
+            container.Resolve<IToy>();
+        }
+
+        [TestMethod]
+        public void GetsInstanceOfInterfaceTypeSuccesfully()
+        {
+            container.RegisterType<IMaterial, Plastic>();
+            var subject = container.Resolve<IMaterial>();
+
+            Assert.IsInstanceOfType(subject, typeof(Plastic));
+        }
+
+        [TestMethod]
+        public void InitializeFieldWithSpecifiedPlasticTypeSuccessfully()
+        {
+            container.RegisterType<IMaterial, Plastic>();
+            container.RegisterType<IToy, Toy>();
+            var subject = (Toy) container.Resolve<IToy>();
+
+            Assert.IsInstanceOfType(subject.plastic, typeof(Plastic));
+        }
+
+        [TestMethod]
+        public void RegistersGuidGeneratorSingletonLifetimeSuccessfully()
+        {
+            container.RegisterType<IGuidGenerator, GuidGenerator>(LifeTime.Singleton);
+
+            var obj1 = (GuidGenerator)container.Resolve<IGuidGenerator>();
+            var obj2 = (GuidGenerator)container.Resolve<IGuidGenerator>();
+
+            Assert.AreEqual(obj1.Guid, obj2.Guid);
+        }
+
+
+        [TestMethod]
+        public void RegisterToySingletonLifetimeSuccessfully()
+        {
+            container.RegisterType<IMaterial, Plastic>();
+            container.RegisterType<IToy, Toy>(LifeTime.Singleton);
+
+            var obj1 = (Toy)container.Resolve<IToy>();
+            var obj2 = (Toy)container.Resolve<IToy>();
+
+            Assert.AreEqual(obj1, obj2);
+        }
+
+
+        [TestMethod]
+        public void RegistersGuidGeneratorTransientLifetimeSuccessfully()
+        {
+            container.RegisterType<IGuidGenerator, GuidGenerator>();
+
+            var obj1 = (GuidGenerator)container.Resolve<IGuidGenerator>();
+            var obj2 = (GuidGenerator)container.Resolve<IGuidGenerator>();
+
+            Assert.AreNotEqual(obj1.Guid, obj2.Guid);
+        }
+
+
+        [TestMethod]
+        public void RegisterToyTransientLifetimeSuccessfully()
+        {
+            container.RegisterType<IMaterial, Plastic>();
+            container.RegisterType<IToy, Toy>();
+
+            var obj1 = (Toy)container.Resolve<IToy>();
+            var obj2 = (Toy)container.Resolve<IToy>();
+
+            Assert.AreNotEqual(obj1, obj2);
+        }
+
 
         public class A { };
         public class B
         {
-            public readonly A clasa;
+            public readonly A a;
 
-            public B(A clasa)
+            public B(A a)
             {
-                this.clasa = clasa;
+                this.a = a;
             }
         }
 
         public interface IMaterial
         {
-          int Weight { get; }
+            int Weight { get; }
         }
 
         public class Plastic : IMaterial
@@ -46,60 +145,14 @@ namespace IoC.Tests
             }
         }
 
-        [TestInitialize]
-        public void Initialize()
+        public interface IGuidGenerator
         {
-            container = new Container();
+            Guid Guid { get; set; }
         }
 
-        [TestMethod]
-        public void CreatesAInstanceWithNoParams()
+        public class GuidGenerator : IGuidGenerator
         {
-            var subject = (A)container.GetInstance(typeof(A));
-            Assert.IsInstanceOfType(subject, typeof(A));
+            public Guid Guid { get; set; } = Guid.NewGuid();
         }
-
-        [TestMethod]
-        public void InitializeFieldWithSpecifiedAType()
-        {
-            var subject = (B)container.GetInstance(typeof(B));
-            Assert.IsInstanceOfType(subject.clasa, typeof(A));
-        }
-
-        [TestMethod]
-        public void GenericGetInstanceMethod()
-        {
-            var subject = (B) container.GetInstance<B>();
-            Assert.IsInstanceOfType(subject.clasa, typeof(A));
-        }
-        
-        [TestMethod]
-        [ExpectedException(typeof(Exception),"Already registered")]
-        public void ThrowsExceptionIfTypeAlreadyRegistered()
-        {
-            container.RegisterType<IMaterial, Plastic>();
-            container.RegisterType<IMaterial, Plastic>();
-        }
-
-        [TestMethod]
-        public void GetsInstanceOfInterfaceType()
-        {
-            container.RegisterType<IMaterial, Plastic>();
-            var subject = container.GetInstance<IMaterial>();
-
-            Assert.IsInstanceOfType(subject, typeof(Plastic));
-        }
-
-        [TestMethod]
-        public void InitializeFieldWithSpecifiedPlasticType()
-        {
-            container.RegisterType<IMaterial, Plastic>();
-            container.RegisterType<IToy, Toy>();
-            var subject = (Toy) container.GetInstance<IToy>();
-
-            Assert.IsInstanceOfType(subject.plastic, typeof(Plastic));
-        }
-    }
-
-    
+    }   
 }
--- IoC/Container.cs
+++ IoC/Container.cs
@@ -1,49 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace IoC
 {
     public class Container
     {
-        private readonly Dictionary<Type, Func<object>> registeredTypes;
+        private readonly Dictionary<Type, Lazy<TypeDefinition>> registeredTypes;
 
         public Container()
         {
-            registeredTypes = new Dictionary<Type, Func<object>>();
+            registeredTypes = new Dictionary<Type, Lazy<TypeDefinition>>();
         }
 
-        public void RegisterType<TInterface,TType>() where TType : TInterface
+        public void RegisterType<TInterface, TType>(LifeTime lifeTime = LifeTime.Transient) where TType : TInterface
         {
             if (registeredTypes.ContainsKey(typeof(TInterface)))
             {
                 throw new Exception("Already registered");
             }
 
-            registeredTypes.Add(typeof(TInterface), () => GetInstance<TType>());
+            registeredTypes.Add(typeof(TInterface), new Lazy<TypeDefinition>(() => GetTypeDefinition(typeof(TType), lifeTime)));
         }
-
-        public object GetInstance<T>()
+     
+        public object Resolve<T>()
         {
-            return GetInstance(typeof(T));
-        }
-
-        public object GetInstance(Type type)
-        {
-            if(registeredTypes.ContainsKey(type))
+            if (registeredTypes.ContainsKey(typeof(T)))
             {
-                return registeredTypes[type]();
+                return registeredTypes[typeof(T)].Value.Constructor();
+            }
+            else
+            {
+                throw new Exception("Type not registered");
+            }
+        }
+       
+        private TypeDefinition GetTypeDefinition(Type type, LifeTime lifeTime)
+        {
+            if (registeredTypes.ContainsKey(type))
+            {
+                return registeredTypes[type].Value;
             }
 
-            var constructor = type.GetConstructors()
-                .OrderByDescending(x => x.GetParameters().Length)
-                .First();
+            TypeDefinition registeredType = new TypeDefinition();
+            registeredType.Type = type;
+            registeredType.LifeTime = lifeTime;
 
-            var args = constructor.GetParameters()
+            ConstructorInfo constructor = type.GetConstructors()
+              .OrderByDescending(x => x.GetParameters().Length)
+              .First();
+
+            object[] args = constructor.GetParameters()
                 .Select(x => GetInstance(x.ParameterType))
                 .ToArray();
 
-            return Activator.CreateInstance(type, args);
+            Func<object> lambda = null;
+
+            if(lifeTime == LifeTime.Transient)
+            {
+                Expression objArguments = Expression.Constant(args);
+                Expression objType = Expression.Constant(type);
+                Expression call = Expression.Call(typeof(Activator).GetMethod("CreateInstance", new[] { typeof(Type), typeof(object[]) } ), objType, objArguments);
+
+                lambda = Expression.Lambda<Func<object>>(call).Compile();
+            }
+
+            if (lifeTime == LifeTime.Singleton)
+            {
+                object obj = Activator.CreateInstance(type, args);
+                lambda = () => obj;
+            }
+          
+            registeredType.Constructor = lambda;
+
+            return registeredType;
+        }
+
+        private object GetInstance(Type type)
+        {
+            if (registeredTypes.ContainsKey(type))
+            {
+                return registeredTypes[type].Value.Constructor();
+            }
+            else
+            {
+                throw new Exception("Parameters not registered");
+            }
+        }
+
+        public enum LifeTime
+        {
+            Transient = 0,
+            Singleton = 1
+        }
+
+        public class TypeDefinition
+        {
+            public Type Type { get; set; }
+            public LifeTime LifeTime { get; set; }
+            public Func<object> Constructor { get; set; }
         }
     }
 }
